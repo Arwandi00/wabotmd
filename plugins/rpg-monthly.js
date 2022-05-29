3b40c8b8ad9b9bc1bebff600dd94940ca82a2ba0
Arwandi00 <100402870+Arwandi00@users.noreply.github.com>
2022-05-29 20:35:27 +0800

    Add files via upload

--- plugins/rpg-monthly.js
+++ plugins/rpg-monthly.js
@@ -1,16 +1,14 @@
-import db from '../lib/database.js'
-
 const rewards = {
     exp: 50000,
-    money: 49999,
+    money: 70000,
     potion: 10,
-    mythic: 3,
-    legendary: 1
+    mythic: 40,
+    legendary: 15
 }
 
-const cooldown = 2592000000
+const cooldown = 7000
 let handler = async (m) => {
-    let user = db.data.users[m.sender]
+    let user = global.db.data.users[m.sender]
     if (new Date - user.lastmonthly < cooldown) throw `You have already claimed this monthly claim, wait for *${((user.lastmonthly + cooldown) - new Date()).toTimeString()}*`
     let text = ''
     for (let reward of Object.keys(rewards)) if (reward in user) {
@@ -23,6 +21,7 @@ let handler = async (m) => {
 handler.help = ['monthly']
 handler.tags = ['rpg']
 handler.command = /^(monthly)$/i
+handler.private = true
 
 handler.cooldown = cooldown
 
