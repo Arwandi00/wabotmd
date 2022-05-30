000c777b7703b3d3a13a7473c96216de8c954a19
Arwandi00 <100402870+Arwandi00@users.noreply.github.com>
2022-05-30 12:32:30 +0800

    Add files via upload

---
+++ image-gimage.js
@@ -0,0 +1,16 @@
+import { googleImage } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
+    const res = await googleImage(text)
+    conn.sendFile(m.chat, res.getRandom(), 'gimage.jpg', `
+*── 「 GOOGLE IMAGE 」 ──*
+
+Result from *${text}*
+`.trim(), m)
+}
+handler.help = ['Khusus Owner']
+handler.tags = ['owner']
+handler.command = /^(gimage1|image1)$/i
+handler.owner = true
+
+export default handler
